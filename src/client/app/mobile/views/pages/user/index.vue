06397b1a629f9096ff5bed3d703a50b0e3928ca0
Xeltica <xeltica@gmail.com>
2019-07-28 22:48:16 +0900

    solve #31

--- src/client/app/mobile/views/pages/user/index.vue
+++ src/client/app/mobile/views/pages/user/index.vue
@@ -46,7 +46,7 @@
 						<fa icon="map-marker"/>{{ user.location }}
 					</p>
 					<p class="birthday" v-if="user.host === null && user.birthday">
-						<fa icon="birthday-cake"/>{{ user.birthday.replace('-', '年').replace('-', '月') + '日' }} ({{ $t('years-old', { age }) }})
+						<fa icon="birthday-cake"/>{{ birthday }} ({{ $t('years-old', { age }) }})
 					</p>
 				</div>
 				<div class="status">
@@ -95,6 +95,7 @@ import XHome from './home.vue';
 import { getStaticImageUrl } from '../../../../common/scripts/get-static-image-url';
 import XIntegrations from '../../../../common/views/components/integrations.vue';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import { isBirthday } from '../../../../../../misc/birthday';
 
 export default Vue.extend({
 	i18n: i18n('mobile/views/pages/user.vue'),
@@ -125,6 +126,17 @@ export default Vue.extend({
 				backgroundColor: this.user.bannerColor,
 				backgroundImage: `url(${ this.user.bannerUrl })`
 			};
+		},
+		birthday() {
+			if (!this.user.birthday)
+				return null;
+
+			const b = this.user.birthday.split('-');
+			if (isBirthday(Number(b[1]), Number(b[2]))) {
+				return this.$t('happy-birthday');
+			} else {
+				return this.user.birthday.replace('-', this.$t('year')).replace('-', this.$t('month')) + this.$t('day');
+			}
 		}
 	},
 	watch: {
